8f71ab505f14b11dd622a150ec9c89111b89bf9a
francoz123 <79244951+francoz123@users.noreply.github.com>
2023-08-11 09:17:41 +1000

    Update connect_four_environment.py

--- src/connect_four_environment.py
+++ src/connect_four_environment.py
@@ -10,13 +10,20 @@ class ConnectFourEnvironment(ConnectFourBaseEnvironment):
     # Note: in a static method you do not have access to self
     def get_legal_actions(game_state):
         # it must return the legal actions for the current game state
-        return []
+        actions = []
+        gb = game_state['game-board']
+        for i in range(gb.get_width):
+							    if ConnectFourBaseEnvironment.is_valid_column(gb, i) and !ConnectFourBaseEnvironment.is_column_full(gb, i):
+                for option in  ['release-', 'popup-', 'use-power-up-']:
+                    actions.append(option + i)
+        return actions
     
     # TODO
     # static method
     # Note: in a static method you do not have access to self
     def is_terminal(game_state):
         # it must return True if there is a winner or there are no more legal actions, False otherwise
+        gb = 
         return False
     
     # TODO
